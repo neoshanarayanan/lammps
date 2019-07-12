e3346fce13c5d91875754bf992c72772203dcfa5
neoshanarayanan <neosha@mit.edu>
2019-07-12 15:05:28 -0400

    plotted strain due to tensile stress

--- tensile_deformation/analysis.py
+++ tensile_deformation/analysis.py
@@ -5,7 +5,7 @@
 import matplotlib.pyplot as plt
 
 
-f = open("log.lammps", "r")
+f = open("log.PE_nc10_cl1000.txt", "r")
 
 timesteps = []
 temps = []
@@ -16,7 +16,7 @@ data_lines = []
 for i, line in enumerate(f.readlines()):
     linenum = i+1
     
-    if linenum >= 63 and linenum <= 89:
+    if linenum >= 10898 and linenum <= 12617:
         data_lines.append(str.split(line))
 
 headers = data_lines[0]
@@ -31,24 +31,27 @@ for i, header in enumerate(headers):
 
     dataDict.update({header: column})
 
-print(dataDict)
-
 
 # dataDict shows each column stored in a list identified by header which is the key
 
 
 # plot temp vs volume
 
-temp = dataDict['KinEng']
-volume = dataDict['Volume']
+
+
+param1 = "time"
+param2 = "zstrain"
+
+x = dataDict['Step']
+y = dataDict['Pzz']
 
 fig = plt.figure()
-plt.plot(temp, volume)
-fig.suptitle('volume vs temp', fontsize=15)
-plt.xlabel('kineng', fontsize=10)
-plt.ylabel('volume', fontsize=10)
+plt.plot(x, y)
+fig.suptitle(param1 + ' vs '+ param2, fontsize=15)
+plt.xlabel(param1, fontsize=10)
+plt.ylabel(param2, fontsize=10)
 
-fig.savefig('tempvsvolume.png')
+fig.savefig(param2 + '_t.png')
 plt.show()
 
 f.close()
